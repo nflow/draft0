5d809d5137e5bdcfd586edc373217f7856be805d
Florian Niederhuber <florian.niederhuber@onelogic.de>
2021-03-20 23:26:00 +0100

    Menu tabs

--- Assets/Scripts/UI/GUIInputHandler.cs
+++ Assets/Scripts/UI/GUIInputHandler.cs
@@ -3,19 +3,7 @@ using UnityEngine.UI;
 
 public class GUIInputHandler : MonoBehaviour
 {
-    public GameObject buildMenu;
     public GameObject placementGrid;
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.B)) {
-            ToggleBuildMenu();
-        }
-    }
-
-    public void ToggleBuildMenu() {
-        buildMenu.SetActive(!buildMenu.activeSelf);
-    }
 
     public void Build(Building building) {
         PlacementGrid script = placementGrid.GetComponent<PlacementGrid>();
